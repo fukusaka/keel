31ececa3eda9bf7b384b32282afdee057ea12df8
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2026-03-15 22:49:28 +0900

    feat(nwconnection): add engine-nwconnection module to settings and configure macOS targets

---
+++ engine-nwconnection/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    macosArm64 {
+        compilations["main"].cinterops {
+            create("nwconnection") {
+                defFile("src/nativeInterop/cinterop/nwconnection.def")
+            }
+        }
+    }
+    macosX64 {
+        compilations["main"].cinterops {
+            create("nwconnection") {
+                defFile("src/nativeInterop/cinterop/nwconnection.def")
+            }
+        }
+    }
+
+    // Creates macosMain intermediate source set shared by macosArm64 and macosX64
+    applyDefaultHierarchyTemplate()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":core"))
+            }
+        }
+        val macosTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
