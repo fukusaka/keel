5afbcfb6afd6087f557ed7bdf5b37569872b198e
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2026-03-19 22:19:11 +0900

    build(netty): add coroutines test dependencies
    Add kotlinx-coroutines-core and kotlinx-coroutines-test to jvmTest
    for runBlocking in IoEngine conformance tests.

--- engine-netty/build.gradle.kts
+++ engine-netty/build.gradle.kts
@@ -19,6 +19,8 @@ kotlin {
         jvmTest {
             dependencies {
                 implementation(kotlin("test"))
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlinx.coroutines.test)
             }
         }
     }
