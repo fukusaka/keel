36f4c27c8e4d108210c260acd35eaa4b76564d62
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2026-03-23 03:20:32 +0900

    fix(engine-epoll): cancel pending accept on ServerChannel close
    Track the pending accept continuation and resume it with
    CancellationException on close(). Same pattern as kqueue fix.

--- engine-epoll/src/linuxMain/kotlin/io/github/fukusaka/keel/engine/epoll/EpollServerChannel.kt
+++ engine-epoll/src/linuxMain/kotlin/io/github/fukusaka/keel/engine/epoll/EpollServerChannel.kt
@@ -5,6 +5,8 @@ import io.github.fukusaka.keel.core.Channel
 import io.github.fukusaka.keel.core.ServerChannel
 import io.github.fukusaka.keel.core.SocketAddress
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.suspendCancellableCoroutine
 import platform.posix.EAGAIN
 import platform.posix.accept
@@ -43,6 +45,7 @@ internal class EpollServerChannel(
 ) : ServerChannel {
 
     private var _active = true
+    private var pendingAcceptCont: CancellableContinuation<Unit>? = null
 
     override val isActive: Boolean get() = _active
 
@@ -69,11 +72,14 @@ internal class EpollServerChannel(
             if (err == EAGAIN) {
                 // Suspend until EventLoop reports serverFd is readable
                 suspendCancellableCoroutine<Unit> { cont ->
+                    pendingAcceptCont = cont
                     eventLoop.register(serverFd, EpollEventLoop.Interest.READ, cont)
                     cont.invokeOnCancellation {
+                        pendingAcceptCont = null
                         eventLoop.unregister(serverFd, EpollEventLoop.Interest.READ)
                     }
                 }
+                pendingAcceptCont = null
                 continue
             }
             error("accept() failed: errno=$err")
@@ -83,6 +89,10 @@ internal class EpollServerChannel(
     override fun close() {
         if (_active) {
             _active = false
+            pendingAcceptCont?.resumeWithException(
+                CancellationException("ServerChannel closed"),
+            )
+            pendingAcceptCont = null
             close(serverFd)
         }
     }
