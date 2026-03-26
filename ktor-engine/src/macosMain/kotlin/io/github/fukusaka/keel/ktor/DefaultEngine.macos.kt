c2a442436f678bf289f917a0ebf16c88001bb63b
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2026-03-27 03:40:39 +0900

    feat(ktor-engine): use pool allocators as default for all platforms
    Default engines now use SlabAllocator (kqueue/epoll) and
    PooledDirectAllocator (NIO) instead of HeapAllocator. Per-EventLoop
    pooling is automatic via createForEventLoop().

--- ktor-engine/src/macosMain/kotlin/io/github/fukusaka/keel/ktor/DefaultEngine.macos.kt
+++ ktor-engine/src/macosMain/kotlin/io/github/fukusaka/keel/ktor/DefaultEngine.macos.kt
@@ -1,6 +1,8 @@
 package io.github.fukusaka.keel.ktor
 
 import io.github.fukusaka.keel.core.IoEngine
+import io.github.fukusaka.keel.core.IoEngineConfig
 import io.github.fukusaka.keel.engine.kqueue.KqueueEngine
+import io.github.fukusaka.keel.io.SlabAllocator
 
-internal actual fun defaultEngine(): IoEngine = KqueueEngine()
+internal actual fun defaultEngine(): IoEngine = KqueueEngine(IoEngineConfig(allocator = SlabAllocator()))
